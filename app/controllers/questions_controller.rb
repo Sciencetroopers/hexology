669399c25291db2371937ec95f17cade3a42be22
Sciencetroopers <mehta.paritosh95@gmail.com>
2017-03-16 15:09:33 +0530

    Questions CRUD

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @questions = Question.all.order('created_at ASC')
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question, :answer)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+end
