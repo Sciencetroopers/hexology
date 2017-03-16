c1c7b6ffcdb92e923ab1272689488fe32595052d
Sciencetroopers <mehta.paritosh95@gmail.com>
2017-03-16 19:35:44 +0530

    Answer Check

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,21 @@ class PagesController < ApplicationController
     @q_id = current_user.question_id
     @question = Question.find(@q_id)
   end
+
+  def check
+    @q_id = current_user.question_id
+    @question = Question.find(@q_id)
+    @answer = params[:answer]
+    if @answer == @question.answer
+      current_user.update_attribute(:question_id, @q_id + 1)
+      if current_user.question_id == 16
+        redirect_to pages_winner_path
+      else
+        redirect_to pages_question_path
+      end
+    else
+      render 'question'
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questions
   get 'pages/question'
+  post 'pages/check'
   get 'pages/contact'
   get 'pages/rules'
+  get 'pages/winner'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
